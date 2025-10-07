16671c3360f89faca3be93e5ff17ba2159de3d14
Nicram123 <141836446+Nicram123@users.noreply.github.com>
2025-10-08 01:26:06 +0200

    Update zad5h.sql

--- 01_setup/zad5h.sql
+++ 01_setup/zad5h.sql
@@ -1,1 +1,7 @@
+SELECT p.imie, p.nazwisko
+FROM pracownicy p
+JOIN wynagrodzenie w ON p.id_pracownika = w.id_pracownika
+JOIN godziny g ON w.id_godziny = g.id_godziny
+JOIN premia pr ON w.id_premii = pr.id_premii
+WHERE g.liczba_godzin > 160 AND pr.kwota = 0;
 
