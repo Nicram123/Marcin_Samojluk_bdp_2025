5a236b4e59a3ac2d915977864fc18a0ff92ea744
Nicram123 <141836446+Nicram123@users.noreply.github.com>
2025-10-08 01:24:47 +0200

    Update zad5g.sql

--- 01_setup/zad5g.sql
+++ 01_setup/zad5g.sql
@@ -1,1 +1,6 @@
+SELECT p.imie, p.nazwisko
+FROM pracownicy p
+JOIN wynagrodzenie w ON p.id_pracownika = w.id_pracownika
+JOIN pensja pe ON w.id_pensji = pe.id_pensji
+WHERE pe.kwota BETWEEN 1500 AND 3000;
 
