1f29d2feabe1b720e8150c2ea8194111c8dbd68d
Nicram123 <141836446+Nicram123@users.noreply.github.com>
2025-10-08 01:30:27 +0200

    Update zad5p.sql

--- 01_setup/zad5p.sql
+++ 01_setup/zad5p.sql
@@ -1,1 +1,9 @@
+DELETE FROM pracownicy
+WHERE id_pracownika IN (
+    SELECT p.id_pracownika
+    FROM pracownicy p
+    JOIN wynagrodzenie w ON p.id_pracownika = w.id_pracownika
+    JOIN pensja pe ON w.id_pensji = pe.id_pensji
+    WHERE pe.kwota < 1200
+);
 
