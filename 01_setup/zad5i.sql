7b7398096065e5950b223ef243110941813af078
Nicram123 <141836446+Nicram123@users.noreply.github.com>
2025-10-08 01:27:43 +0200

    Update zad5i.sql

--- 01_setup/zad5i.sql
+++ 01_setup/zad5i.sql
@@ -1,1 +1,6 @@
+SELECT p.imie, p.nazwisko, pe.kwota
+FROM pracownicy p
+JOIN wynagrodzenie w ON p.id_pracownika = w.id_pracownika
+JOIN pensja pe ON w.id_pensji = pe.id_pensji
+ORDER BY pe.kwota ASC;
 
