accde94145bc9f27bdf0cd0399cc06bf7b681f0b
Nicram123 <141836446+Nicram123@users.noreply.github.com>
2025-10-08 01:29:53 +0200

    Update zad5n.sql

--- 01_setup/zad5n.sql
+++ 01_setup/zad5n.sql
@@ -1,1 +1,6 @@
+SELECT pe.stanowisko, SUM(pe.kwota + pr.kwota) AS suma_wynagrodzen
+FROM wynagrodzenie w
+JOIN pensja pe ON w.id_pensji = pe.id_pensji
+JOIN premia pr ON w.id_premii = pr.id_premii
+GROUP BY pe.stanowisko;
 
