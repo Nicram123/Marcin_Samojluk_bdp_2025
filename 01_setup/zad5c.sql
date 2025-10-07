75d0b7a6092ab9ed4b1199cca261bebe943cdfb9
Nicram123 <141836446+Nicram123@users.noreply.github.com>
2025-10-08 01:22:10 +0200

    Update zad5c.sql

--- 01_setup/zad5c.sql
+++ 01_setup/zad5c.sql
@@ -1,1 +1,7 @@
+SELECT p.id_pracownika
+FROM wynagrodzenie w
+JOIN pensja pe ON w.id_pensji = pe.id_pensji
+JOIN premia pr ON w.id_premii = pr.id_premii
+JOIN pracownicy p ON w.id_pracownika = p.id_pracownika
+WHERE pr.kwota = 0 AND pe.kwota > 2000;
 
