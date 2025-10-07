735c79d6c91e2787a3f045e6a62e1c41034a3d47
Nicram123 <141836446+Nicram123@users.noreply.github.com>
2025-10-08 01:30:10 +0200

    Update zad5o.sql

--- 01_setup/zad5o.sql
+++ 01_setup/zad5o.sql
@@ -1,1 +1,7 @@
+SELECT pe.stanowisko, COUNT(pr.id_premii) AS liczba_premii
+FROM wynagrodzenie w
+JOIN pensja pe ON w.id_pensji = pe.id_pensji
+JOIN premia pr ON w.id_premii = pr.id_premii
+WHERE pr.kwota > 0
+GROUP BY pe.stanowisko;
 
