83862ce64ca859f750439b075d8210ac28bce8dd
Marcin Samojluk <141836446+Nicram123@users.noreply.github.com>
2025-10-22 01:06:10 +0200

    Update zad4.sql

--- 03/zad4.sql
+++ 03/zad4.sql
@@ -1,1 +1,12 @@
+-- 4 
 
+CREATE TABLE input_points (
+  id SERIAL PRIMARY KEY,
+  geom geometry(Point, 4326)
+);
+
+-- ST_MakePoint - tworzy punkt , ST_SetSRID - ustawia epsg 
+INSERT INTO input_points (geom)
+VALUES
+  (ST_SetSRID(ST_MakePoint(8.36093, 49.03174), 4326)),
+  (ST_SetSRID(ST_MakePoint(8.39876, 49.00644), 4326));
