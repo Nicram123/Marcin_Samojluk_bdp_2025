52cdf252ffe8a68bcec85c6b43002651aee4de39
Marcin Samojluk <141836446+Nicram123@users.noreply.github.com>
2025-10-22 14:04:22 +0200

    Update zad5.sql

--- 05/zad5.sql
+++ 05/zad5.sql
@@ -1,1 +1,7 @@
-
+-- WHERE NOT ST_HasArc(geometry) - tylko takie nie zawierające łuków
+-- ST_Force3D(geometry) - każdą geometrie do 3D przekształca 
+-- ST_Union - wszystkie geometrie łączy w jedną wspólną 
+-- ST_Buffer - bufor o promieniu 5 na złaczonej geometrii 
+SELECT ST_Area(ST_Buffer(ST_Union(ST_Force3D(geometry)), 5)) 
+FROM obiekty
+WHERE NOT ST_HasArc(geometry)
